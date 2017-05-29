023d0166ae92fc90ed049431787a9b39a0043c4a
Lars Verhulst <lars.verhulst@hotmail.com>
2017-05-29 11:09:09 +0200

    Client layout changed

--- Netwerk Programmeren Chat-App-Client/Client/src/ClientPanel.java
+++ Netwerk Programmeren Chat-App-Client/Client/src/ClientPanel.java
@@ -1,4 +1,8 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.MessageFormat;
 
 /**
  * Created by Ian on 8-5-2017.
@@ -6,7 +10,38 @@ import javax.swing.*;
 public class ClientPanel extends JPanel {
     private ClientUser user;
 
+    private JTextArea messageSend;
+    private JTextArea messageView;
+
+    private Button sendButton;
+
     public ClientPanel() {
+        super(new BorderLayout());
+        JPanel southPanel = new JPanel(new FlowLayout());
+        messageSend = new JTextArea(4 , 20);
+        messageSend.setPreferredSize(new Dimension(getWidth()-50 , 40));
+        messageSend.setSize(getWidth()-50 , 40);
+        messageSend.setLineWrap(true);
+        JScrollPane scroll = new JScrollPane(messageSend, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS , JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        //scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        southPanel.add(scroll);
+
+        messageView = new JTextArea(20 , 20);
+        add(new JScrollPane(messageView) , BorderLayout.NORTH);
+
+        sendButton = new Button("SEND");
+        sendButton.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String message = messageSend.getText();
+                messageSend.setText("");
+
+
+            }
+        });
+        southPanel.add(sendButton);
+        add(southPanel , BorderLayout.SOUTH);
         user = new ClientUser();
     }
 
